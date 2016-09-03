4f4ed1a8f4b293ab8f633c0da0a2d4b1f4d22382
hch1011 <hch1011@163.com>
2016-09-03 13:33:47 +0800

    server

---
+++ meter-web/src/main/java/com/jd/meter/sync/handler/MetterDispatcherHander.java
@@ -0,0 +1,13 @@
+package com.jd.meter.sync.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * Created by hujintao on 2016/8/6.
+ */
+public class MetterDispatcherHander extends ChannelInboundHandlerAdapter {
+   
+}
